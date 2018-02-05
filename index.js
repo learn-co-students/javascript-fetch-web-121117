8cf648bd5ed740721d04e43ee9464aca22361792
johneckert <johnteckert@gmail.com>
2018-02-05 15:05:04 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much.";
+//XHR
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.github.com/repos/jquery/jquery/commits");
+xhr.responseType = "json";
+
+xhr.onload = function() {
+  console.log(xhr.response);
+};
+
+xhr.onerror = function() {
+  console.log("Booo");
+};
+
+xhr.send();
+
+//fetch
+
+fetch("https://api.github.com/repos/jquery/jquery/commits")
+  .then(res => res.json())
+  .then(json => console.log(json));
+
+//fetch with Auth
+
+const token = "YOUR_TOKEN_HERE";
+fetch("https://api.github.com/user/repos", {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
