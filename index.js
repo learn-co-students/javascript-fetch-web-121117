51bb8e34f448a0aa50415c5378cd1dede4029eee
olegchursin <olegchursin@gmail.com>
2018-02-06 11:31:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const app = "I don't do much.";
 
-const token = '4aee8d34c93c9dd2fe2b47a62c3e438dbb1e5d96'
+const token = ''
 fetch('https://api.github.com/olegchursin/repos', {
   headers: {
     Authorization: `token ${token}`
